5b04e10354fe17a16e9a0f5d99dde02e48de1690
ad0ll <20057155+ad0ll@users.noreply.github.com>
2024-05-07 19:00:48 -0400

    wrap up token card

--- components/token_card.tsx
+++ components/token_card.tsx
@@ -4,47 +4,40 @@ import {Progress} from "@/components/ui/progress";
 
 export function TokenCard() {
     return (
-        <Card>
-            <CardContent>
-                <div className={"flex space-x-4 border-4"}>
-                    <Image
-                        src="/sui-overflow-2024.jpg"
-                        alt="Vercel Logo"
-                        className="dark:invert"
-                        width={80}
-                        height={80}
-                        priority
-                    />
-                    <div className={"flex-col space-y-2"}>
-                        <div className={"flex space-x-4"}>
-                            <div className={"flex-col"}>
-                                <p className={"text-xl"}>
-                                    Indiana Jones
-                                </p>
-                                <p className={"text-muted-foreground text-muted-foreground text-sm"}>
-                                    ticker: $JONES
-                                </p>
-                            </div>
-                            <div className={"flex-col text-center"}>
-                                <p className={"text-sm text-green-400"}>
-                                    Market Cap
-                                </p>
-                                <p className={"text-md text-green-400"}>
-                                    $1,000,000,000
-                                </p>
-                            </div>
+        <Card className="p-4">
+            <div className="flex space-x-4 max-w-md items-center">
+                <Image
+                    src="/sui-overflow-2024.jpg" //TODO dynamic image from on-chain config
+                    alt="Vercel Logo" //TODO dynamic alt text
+                    className="w-32 h-32 flex border-amber-400 border-2"
+                    width={100}
+                    height={100}
+                />
+                <div className="flex-col space-y-2">
+                    <div className="flex space-x-4 justify-between">
+                        <div className="flex-col">
+                            <p className="text-xl">Indiana Jones</p>
+                            <p className="text-muted-foreground  text-sm">
+                                $JONES
+                            </p>
                         </div>
-                        <Progress value={66}/>
-                        <p className={"text-muted-foreground"}>
-                            Indiana Jones is an American media franchise consisting of five films and a prequel
-                            television series, along with games, comics, and tie-in novels, that depicts the adventures
-                            of Dr. Henry Walton "Indiana" Jones, Jr. (portrayed in all films by Harrison Ford), a
-                            fictional professor of archaeology.
+                        <div className="flex-col text-center ml-auto">
+                            <p className="text-sm text-green-400">Market Cap</p>
+                            <p className="text-md text-green-400">$1,000,000,000</p>
+                        </div>
+                    </div>
+                    <Progress value={66} />
+                    <div className="h-18 overflow-hidden">
+                        <p className="text-muted-foreground text-xs line-clamp-3">
+                            Indiana Jones is an American media franchise consisting of five films
+                            and a prequel television series, along with games, comics, and tie-in
+                            novels, that depicts the adventures of Dr. Henry Walton "Indiana"
+                            Jones, Jr. (portrayed in all films by Harrison Ford), a fictional
+                            professor of archaeology.
                         </p>
                     </div>
                 </div>
-            </CardContent>
-
+            </div>
         </Card>
     )
 }
