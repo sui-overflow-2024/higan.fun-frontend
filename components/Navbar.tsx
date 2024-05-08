22a9c86f32219e79a48ae8e047001ab3e0fff143
ad0ll <20057155+ad0ll@users.noreply.github.com>
2024-05-08 19:21:37 -0400

    Functioning but unstyled sui wallet connector

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,91 +1,39 @@
+'use client';
 import Link from 'next/link';
-import Image from "next/image";
+import {ConnectButton, useCurrentAccount} from '@mysten/dapp-kit';
 
 export default function Navbar() {
+    const account = useCurrentAccount();
+    const shortAddress = account && `${account.address.slice(0, 6)}...${account.address.slice(-6)}`;
+    console.log(account)
     return (
         <nav className="bg-gray-900 border-b-2 border-gray-800">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="flex items-center justify-between h-16">
                     <div className="flex items-center">
-                            <Link href="/">
-                                {/*<Image*/}
-                                {/*    src="/sui-overflow-2024.jpg"*/}
-                                {/*    alt="Vercel Logo"*/}
-                                {/*    className="dark:invert"*/}
-                                {/*    width={100}*/}
-                                {/*    height={24}*/}
-                                {/*    priority*/}
-                                {/*/>*/}
-                            </Link>
+                        <Link href="/">
+                            {/*<Image*/}
+                            {/*    src="/sui-overflow-2024.jpg"*/}
+                            {/*    alt="Vercel Logo"*/}
+                            {/*    className="dark:invert"*/}
+                            {/*    width={100}*/}
+                            {/*    height={24}*/}
+                            {/*    priority*/}
+                            {/*/>*/}
+                        </Link>
                         <div className="hidden md:block">
                             <div className="ml-10 flex items-baseline space-x-4">
-                                <Link href="/" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
-                                        Home
+                                <Link href="/"
+                                      className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+                                    Home
                                 </Link>
                             </div>
                         </div>
+                        <div className={"bg-accent p-3 rounded-xl"}>
+                            {shortAddress}
+                        </div>
+                        <ConnectButton/>
                     </div>
-                    <div className="-mr-2 flex md:hidden">
-                        <button
-                            type="button"
-                            className="bg-gray-900 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
-                            aria-controls="mobile-menu"
-                            aria-expanded="false"
-                        >
-                            <span className="sr-only">Open main menu</span>
-                            {/* Icon when menu is closed. */}
-                            <svg
-                                className="block h-6 w-6"
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke="currentColor"
-                                aria-hidden="true"
-                            >
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth={2}
-                                    d="M4 6h16M4 12h16M4 18h16"
-                                />
-                            </svg>
-                            {/* Icon when menu is open. */}
-                            <svg
-                                className="hidden h-6 w-6"
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke="currentColor"
-                                aria-hidden="true"
-                            >
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth={2}
-                                    d="M6 18L18 6M6 6l12 12"
-                                />
-                            </svg>
-                        </button>
-                    </div>
-                </div>
-            </div>
-
-            {/* Mobile menu, show/hide based on menu state. */}
-            <div className="md:hidden" id="mobile-menu">
-                <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-                    <Link href="/" className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">
-                            Home
-                    </Link>
-                    {/*<Link href="/about">*/}
-                    {/*    <a className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">*/}
-                    {/*        About*/}
-                    {/*    </a>*/}
-                    {/*</Link>*/}
-                    {/*<Link href="/contact">*/}
-                    {/*    <a className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">*/}
-                    {/*        Contact*/}
-                    {/*    </a>*/}
-                    {/*</Link>*/}
                 </div>
             </div>
         </nav>
