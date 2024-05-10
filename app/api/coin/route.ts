361c6641b583ac2237e7207c896291f84f7a6c84
ad0ll <20057155+ad0ll@users.noreply.github.com>
2024-05-10 18:53:30 -0400

    Test backend, tweaks

--- app/api/coin/route.ts
+++ app/api/coin/route.ts
@@ -1,12 +1,27 @@
 import type {NextApiRequest, NextApiResponse} from 'next'
 import {TokenWithAdditionalMetadata} from "@/lib/types";
-import handlebars from 'handlebars';
 import {PrismaClient} from "@prisma/client";
 
 const prisma = new PrismaClient();
-
-export const tokenTemplate = handlebars.compile(`
-module we_hate_the_ui_contracts::{{name_snake_case}} {
+export const dynamic = 'force-dynamic' // defaults to auto
+type TemplateParams = {
+    nameSnakeCase: string,
+    nameSnakeCaseCaps: string,
+    decimals: number,
+    symbol: string,
+    description: string,
+    iconUrl: string,
+}
+const generateTokenTemplate = ({
+                                     nameSnakeCase,
+                                      nameSnakeCaseCaps,
+                                      decimals,
+                                      symbol,
+                                      description,
+                                      iconUrl,
+                               }: TemplateParams) => {
+    return `
+module we_hate_the_ui_contracts::${nameSnakeCase} {
     use std::option;
     use sui::coin::{Self, Coin, TreasuryCap};
     use sui::transfer;
@@ -15,45 +30,53 @@ module we_hate_the_ui_contracts::{{name_snake_case}} {
     /// Name of the coin. By convention, this type has the same name as its parent module
     /// and has no fields. The full type of the coin defined by this module will be COIN<{{token_sname_case_caps}}>.
     /// Note: For some reason the OTW has to be named the same as the address
-    public struct {{name_snake_case_caps}} has drop {}
+    public struct ${nameSnakeCaseCaps} has drop {}
 
     /// Register the managed currency to acquire its TreasuryCap. Because
     /// this is a module initializer, it ensures the currency only gets
     /// registered once.
-    fun init(witness: {{name_snake_case_caps}}, ctx: &mut TxContext) {
+    fun init(witness: ${nameSnakeCaseCaps}, ctx: &mut TxContext) {
         // Get a treasury cap for the coin and give it to the transaction sender
-        let (treasury_cap, metadata) = coin::create_currency<{{name_snake_case_caps}}>(witness, {{decimals}}, b"{{name_snake_case_caps}}", b"{{symbol}}", b"{{description}}", b"{{icon_url}}", ctx);
+        let (treasury_cap, metadata) = coin::create_currency<${nameSnakeCaseCaps}>(witness, ${decimals}, b"${nameSnakeCaseCaps}", b"${symbol}", b"${description}", b"${iconUrl}", ctx);
         transfer::public_freeze_object(metadata);
         transfer::public_transfer(treasury_cap, tx_context::sender(ctx))
     }
 
     /// Manager can mint new coins
     public fun mint(
-        treasury_cap: &mut TreasuryCap<{{name_snake_case_caps}}>, amount: u64, recipient: address, ctx: &mut TxContext
+        treasury_cap: &mut TreasuryCap<${nameSnakeCaseCaps}>, amount: u64, recipient: address, ctx: &mut TxContext
     ) {
         coin::mint_and_transfer(treasury_cap, amount, recipient, ctx)
     }
 
     /// Manager can burn coins
-    public fun burn(treasury_cap: &mut TreasuryCap<{{name_snake_case_caps}}>, coin: Coin<{{name_snake_case_caps}}>) {
+    public fun burn(treasury_cap: &mut TreasuryCap<${nameSnakeCaseCaps}>, coin: Coin<${nameSnakeCaseCaps}>) {
         coin::burn(treasury_cap, coin);
     }
 
-    public fun transfer_cap(treasury_cap: TreasuryCap<{{name_snake_case_caps}}>, target: address){
+    public fun transfer_cap(treasury_cap: TreasuryCap<${nameSnakeCaseCaps}>, target: address){
         //I'm not positive this is secure, in theory: There is only one treasury cap, the person who called init has it,
         // so the only person who should be able to transfer it in the person who called init?
         transfer::public_transfer(treasury_cap, target);
     }
-}`)
-export async function GET(req: NextApiRequest){
-    //TODO Need pagination
-    return prisma.coin.findMany()
+}`
 }
+
+export async function GET(req: NextApiRequest) {
+    //TODO Need pagination
+    // return Response.json({"hello": "world"})
+    const res = await prisma.coin.findMany()
+    return Response.json(res)
+}
+
 export async function POST(
-    req: NextApiRequest,
-    res: NextApiResponse<TokenWithAdditionalMetadata | { error: string }>
+    req: Request,
 ) {
     try {
+        // const body = await req.json()
+        // console.log("demo", body)
+        const body = await req.json();
+        console.log("create token body", body)
         const {
             creator,
             decimals,
@@ -65,25 +88,28 @@ export async function POST(
             twitterUrl,
             discordUrl,
             telegramUrl,
-        } = req.body;
+        } = body as TokenWithAdditionalMetadata;
+        //@voyager, right here
 
+        // console.log("body:", req.body)
+        // console.log("req:", req)
         // @voyager, put your Sui logic to create the coin here
         // If we can't do this on chain, I think the rough steps are:
         // First version: Just create a token, don't worry about collecting a fee
         // 1. Load the token template and populate it with the token details using handlebars
-        const templateData = {
-            name_snake_case_caps: name.replace(" ", "_").toUpperCase(),
-            name_snake_case: name.toLowerCase().replace(" ", "_"),
-            decimals,
-            symbol,
-            description,
-            icon_url: iconUrl,
-        };
-        const tokenCode = tokenTemplate(templateData);
+        // const templateData = {
+        //     nameSnakeCaseCaps: name.replace(" ", "_").toUpperCase(),
+        //     name_snake_case: name.toLowerCase().replace(" ", "_"),
+        //     decimals,
+        //     symbol,
+        //     description,
+        //     icon_url: iconUrl,
+        // };
+        // const tokenCode = tokenTemplate(templateData);
         // 2. See if you can deploy the token code to the chain with the Sui sdk: https://docs.sui.io/guides/developer/app-examples/weather-oracle#initialize-the-project
         // 3. (Later) Register the token with the management contract, and transfer the treasury cap to the management contract
         // Second version, later: User must submit 5 SUI to the manager contract for a fee, which we check here. User submits a signature, we extract the address, check if they paid the fee on chain, and move forward with coin creation if the have.
-        console.log(tokenCode)
+        // console.log(tokenCode)
         const coin = await prisma.coin.create({
             data: {
                 // TODO below should come from Sui deploy
@@ -101,9 +127,9 @@ export async function POST(
                 telegramUrl,
             },
         });
-        return res.status(200).json(coin)
+        return Response.json(coin)
     } catch (err) {
-        console.error("500 error on /createCoin", err)
-        return res.status(500).json({error: `failed to load data ${err}`})
+        console.error(err)
+        return new Response(JSON.stringify({error: `Internal server error: ${err}`}), {status: 500})
     }
 }
--- lib/types.ts
+++ lib/types.ts
@@ -5,6 +5,19 @@ export type Token = {
     iconUrl: string,
     description: string,
 }
+export type NewTokenRequest = {
+    name: string,
+    symbol: string,
+    iconUrl: string,
+    description: string,
+    creator: string,
+    signature: string, //later
+    decimals: number,
+    website: string,
+    twitterUrl: string,
+    discordUrl: string,
+    telegramUrl: string,
+}
 export type TokenWithAdditionalMetadata = Token & {
     creator: string,
     decimals: number,
