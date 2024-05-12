138496571f2d13d0bc9ec461cafe80f39449a434
kyuubisusano <perfectsusano123@gmail.com>
2024-05-13 00:05:53 +0530

    create coin page

---
+++ app/createCoin/page.tsx
@@ -0,0 +1,15 @@
+import Form from "@/components/ui/Form";
+import './style.css'
+import Filter from "@/components/ui/filter";
+const Page = () => {
+  return (
+    <div className=" min-h-[calc(100vh_-_66px)] w-full flex items-center justify-center">
+      
+      <div className=" w-[30%] min-w-[400px] form border-[#34477b] border-2 p-5 rounded-lg">
+        <Form />
+      </div>
+    </div>
+  );
+};
+
+export default Page;
